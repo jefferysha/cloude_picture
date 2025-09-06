f78fd14c4f116055b37abba3a7b6a023b8086aa6
shajunyi <shajunyi@jiayinfintech.cn>
2025-09-06 16:06:11 +0800

    first commit test

---
+++ shapic/src/main/java/com/shapic/shapic/ShapicApplication.java
@@ -0,0 +1,13 @@
+package com.shapic.shapic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShapicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShapicApplication.class, args);
+    }
+
+}
